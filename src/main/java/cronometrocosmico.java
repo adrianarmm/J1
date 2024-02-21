4c2903da1b64514ab7c3033067f3f5e2d62a9a64
adrianarmm <areyemor@myuax.com>
2024-02-21 11:39:19 +0100

    4

--- src/main/java/cronometrocosmico.java
+++ src/main/java/cronometrocosmico.java
@@ -1,12 +1,2 @@
 import java.util.Scanner;
 
-public class cronometrocosmico {
-}
-
-private final double diasEnLaTierra;
-private final double diasEnElNuevoPlaneta;
-
-public cronometrocosmico(double diasEnLaTierra, double diasEnElNuevoPlaneta) {
-    this.diasEnLaTierra = diasEnLaTierra;
-    this.diasEnElNuevoPlaneta = diasEnElNuevoPlaneta;
-}
